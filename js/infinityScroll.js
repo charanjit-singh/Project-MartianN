3de9a2170272bc8dea67e6b582dc0bee3bbe8e31
charanjit-singh <charanjitdotsingh@gmail.com>
2018-04-21 22:33:36 +0530

    Infinity SCroll Implemented

---
+++ js/infinityScroll.js
@@ -0,0 +1,59 @@
+const PostHTML=`
+      <section class="page" style="background-color:#F5F5F5">
+        <div class="container" >
+          <div class="row">
+            <div class="col-md-8 col-md-offset-2" style="background-color:white;">
+              <br>
+              <table width="100%">
+                <tr valign="top">
+                  <td width="30%">
+
+                    <table width="100%"  align="center">
+                      <tr align="left">
+                        <td><img src="images/img01.jpg" width="50px" height="50px" style="border-radius:25px"/></td>
+                        <td>&nbsp;&nbsp;</td>
+                        <td><b>Rohit Koshy</b><br>Finance Guy, Startup Catalyst<br>4h - Edited</td>
+                      </tr>
+                    </table>
+
+                  </td>
+                  <td align="right">
+
+                  <div class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-ellipsis-h" style="font-size:20px"></i></a>
+                    <ul class="dropdown-menu" style="margin-left: 500px;">
+                      <li><a href="#"><i class="fa fa-save" style="width: 25px;"></i>Save Post</a></li>
+                      <li><a href="#"><i class="fa fa-thumbs-o-down" style="width: 25px;"></i>  Not Interested</a></li>
+                      <li><a href="#"><i class="fa fa-eye-slash" style="width: 25px;"></i>  Hide all such posts</a></li>
+                      <li><a href="#"><i class="fa fa-exclamation" style="width: 25px;"></i>  Bad Content</a></li>
+                    </ul>
+                  </div>
+
+                </td>
+              </tr>
+            </table>
+            <p align="left">IIM interviews will be starting soon.<br>What song will the interviewer be singing for the hapless candidates?</p>
+            <p align="right"><a href="#">...See more</a></p>
+
+            <div class="page-description" style="border:1px solid gray">
+              <iframe width="100%" height="400" src="https://www.youtube.com/embed/2z3CEBv8PLI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+              <div style="margin:10px">
+              <h6>Techfest, IIT Bombay | Official Aftermovie 2017-18 (Featuring Sophia, MARNIK, Sountec)</h6>
+              youtube.com<br>
+            </div>
+            </div>
+            <br>
+            <p align="left" style="color:gray"><span class="like-count">1</span> Like(s)  <span class="comment-count"> 1</span> Comment(s)</p>
+            <hr color="gray">
+            <table width="50%">
+              <tr  style="font-size:20px;color:gray">
+                <td class="like-btn"><i class="fa fa-thumbs-o-up"></i>&nbsp;&nbsp;Like</td>
+                <td class="comment-btn"><i class="fa fa-commenting-o"></i>&nbsp;Comment</td>
+                <td class="share-btn"><i class="fa fa-share" ></i>&nbsp;&nbsp;Share</td>
+              </tr>
+            </table>
+           <br>
+         </div>
+         </div>
+        </div>
+      </section>
+`;
